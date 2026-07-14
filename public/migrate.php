bd70c3b0186d8e8b9575dca1590b73e4832933f3
BKK Tarakan <kkp.tarakan.borneo@gmail.com>
2026-07-14 17:37:38 +0800

    Update manual migration script

--- public/migrate.php
+++ public/migrate.php
@@ -4,8 +4,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 $kernel->bootstrap();
 try {
-    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
-    echo "Migrasi Selesai: " . \Illuminate\Support\Facades\Artisan::output();
+    \Illuminate\Support\Facades\Artisan::call('migrate:status');
+    echo "Status Migrasi: \n" . \Illuminate\Support\Facades\Artisan::output();
 } catch (\Exception $e) {
     echo "Gagal migrasi: " . $e->getMessage();
 }
